262ccc829993f1c0219fe6b485b72d741c07361b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-07 15:44:22 +0530

    n2

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,8 +8,8 @@ const AboutMe = () => {
       Greetings Folks!<br></br>
       Meet me...an enthusiastic and forward-thinking Frontend Developer embarking on an exciting journey in the tech realm. My expertise spans HTML, CSS, React, Java, and Spring Boot, with a solid grasp of AWS. I thrive on unraveling complex coding challenges, weaving innovation into every line of code.
 
-Driven by a relentless passion for creating seamless user experiences, I am on a mission to blend creativity with technology. Beyond the screen, I find joy in the adrenaline of gaming, the discipline of combat sports, the thrill of action movies, and the fantastical world of comics.
-
+<p> Driven by a relentless passion for creating seamless user experiences, I am on a mission to blend creativity with technology. Beyond the screen, I find joy in the adrenaline of gaming, the discipline of combat sports, the thrill of action movies, and the fantastical world of comics.
+</p>
 Eager to contribute my skills and bring fresh perspectives to the ever-evolving landscape of web development. Let's collaborate and make a mark in the vibrant tapestry of the developer's industry together.
 
 
